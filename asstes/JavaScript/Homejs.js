5498aaedfa1b5471d60172062d82003ecee90a56
Rayal Khatri <kcrayalgw@gmail.com>
2023-01-08 22:43:47 +0545

    Added Clock and Sub Menu Javascript

--- asstes/JavaScript/Homejs.js
+++ asstes/JavaScript/Homejs.js
@@ -1,1 +1,34 @@
-console.log('Hello');
+function displayTime()
+{
+var dateTime = new Date();
+var hrs=dateTime.getHours();
+var min=dateTime.getMinutes();
+var sec=dateTime.getSeconds();
+var ses = document.getElementById('session');
+if(hrs>12)
+{
+    hrs-=12;
+    ses.textContent='PM'
+}
+else
+{
+    ses.textContent='AM'
+}
+document.getElementById('hrs').innerHTML=hrs
+if(min<10)
+    document.getElementById('min').innerHTML='0'+ min
+else
+    {document.getElementById('min').innerHTML= min}
+if(sec<10)
+    {document.getElementById('sec').innerHTML='0' + sec}
+else
+    {document.getElementById('sec').innerHTML=sec}
+}
+setInterval(displayTime, 10);
+
+let subMenu=document.getElementById('sub-menu')
+let dropDown=document.getElementById('drop-down')
+dropDown.addEventListener('click',(e) =>
+{
+    subMenu.classList.toggle('sub-menu-active')
+})
